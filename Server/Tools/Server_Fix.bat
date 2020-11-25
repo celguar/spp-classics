c902f6e43c49c14355350d9ccd81da7d96006e0d
celguar <celguar@gmail.com>
2020-11-25 04:36:59 +0300

    Server_Fix.bat will now reset settings. Current settings will be copied to Settings_Old folder

--- Server/Tools/Server_Fix.bat
+++ Server/Tools/Server_Fix.bat
@@ -13,25 +13,35 @@ set name=SPP - Classics Collection
 set installpath=SPP_Server
 set branch=master
 cls
-REM echo #########################################################
-REM echo # WARNING!                                              #
-REM echo # All your changes will be undone, including:           #
-REM echo # Server settings                                       #
-REM echo #                                                       #
-REM echo # Back up settings before doing this!                   #
-REM echo #########################################################
-REM echo.
-REM setlocal
-REM :PROMPT
-REM SET /P AREYOUSURE=Are you sure (Y/[N])?
-REM IF /I "%AREYOUSURE%" NEQ "Y" exit
+echo #########################################################
+echo # WARNING!                                              #
+echo # All your changes will be undone, including:           #
+echo # Server settings, Bot settings, AHBot settings         #
+echo # Settings will be copied to Settings_Old folder        #
+echo #                                                       #
+echo # Still it's better to backup!                          #
+echo #########################################################
+echo.
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Are you sure (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" exit
 goto start_restore
 
 :start_restore
 cls
 if not exist "%mainfolder%\%installpath%\launcher.bat" goto not_installed_error
 cd "%mainfolder%\%installpath%"
-..\git\cmd\git.exe reset --hard
+robocopy Settings Settings_Old /E
+..\git\cmd\git.exe reset --hard 5da671e275a5c4b8f432cd55d2d590bed946cdea
+ping -n 5 127.0.0.1>nul
+cls
+echo.
+echo  All files are restored.
+echo  If you had problems with updating please run Server_Update.bat
+echo  to update properly
+echo.
+echo  Old Settings are copied to Settings_Old folder
 echo.
 pause
 exit
